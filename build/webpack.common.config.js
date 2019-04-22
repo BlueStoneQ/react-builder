29a103c5fb40b7cad39e504b707d0bc1fa147255
qiaoyang <qiaoyang@bonc.com.cn>
2019-04-22 10:21:08 +0800

    url-loader

---
+++ build/webpack.common.config.js
@@ -0,0 +1,68 @@
+/**
+ * webpack中dev和prod两种模式公用的配置
+ */
+const path = require('path');
+const config = require('./config');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const ROOT_PATH = config.ROOT_PATH;
+const SRC_PATH = config.SRC_PATH;
+const BUILD_PATH = config.BUILD_PATH;
+
+console.log('process.env.NODE_ENV: ', process.env.NODE_ENV)
+
+module.exports = {
+  entry: path.resolve(SRC_PATH, 'index.js'),
+  output: {
+    path: BUILD_PATH,
+    filename: '[name].[hash:8].bundle.js'
+  },
+  module: {
+    rules: [
+      { 
+        test: /\.js$/,
+        include: path.resolve(SRC_PATH),
+        exclude: path.resolve(ROOT_PATH, 'node_modules'),
+      },
+      {
+        test: /\.(css|less)$/,
+        include: path.resolve(SRC_PATH),
+        exclude: path.resolve(ROOT_PATH, 'node_modules'),
+        use: [ {
+          loader: MiniCssExtractPlugin.loader,
+          options: {
+            // you can specify a publicPath here
+            // by default it uses publicPath in webpackOptions.output
+            publicPath: '../',
+            hmr: process.env.NODE_ENV === 'development', // 根据当前运行环境 决定是否启用样式的热启动
+          },
+        }, 'css-loader', 'postcss-loader', 'less-loader' ]
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        include: path.resolve(ROOT_PATH, 'assets')
+        ,
+        exclude: path.resolve(ROOT_PATH, 'node_modules'),
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8 * 1024, // 图片小于8kb 打包为base64
+              outputPath: 'images'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+    new CleanWebpackPlugin()
+  ]
+};
