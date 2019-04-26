10e317f6a0810c8a100f2f84297239059ce83c2c
qiaoyang <qiaoyang@bonc.com.cn>
2019-04-26 14:29:37 +0800

    DllReferencePlugin - 引入DllPlugin打包的依赖文件

--- build/webpack.dll.config.js
+++ build/webpack.dll.config.js
@@ -4,6 +4,9 @@
  */
 const path = require('path');
 const webpack = require('webpack');
+const config = require('./config');
+
+const ROOT_PATH = config.ROOT_PATH;
 
 module.exports = {
   entry: {
@@ -12,12 +15,13 @@ module.exports = {
   output: {
     filename: '[name].dll.js',
     library: '_dll_[name]', // 防止与全局变量冲突，在manifest参与映射的名称
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(ROOT_PATH, 'dist')
   },
   plugins: [
     new webpack.DllPlugin({
+      context: __dirname,
       name: '_dll_[name]',
-      path: path.resolve(__dirname, 'dist', '[name].manifest.json') // 生成manifest的路径和文件名
+      path: path.resolve(ROOT_PATH, 'dist', 'manifest.json') // 生成manifest的路径和文件名
     })
   ]
 }
