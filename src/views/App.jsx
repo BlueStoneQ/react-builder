42cc170e42e3accb6cc5804975df2be090f0c639
qiaoyang <qiaoyang@bonc.com.cn>
2019-05-05 17:34:07 +0800

    Router-demo-1-基础配置

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -1,9 +1,39 @@
 import React, { Component } from 'react';
 import { hot } from 'react-hot-loader/root';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Header() {
+  return (
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+    </ul>
+  );
+}
 
 class App extends Component {
   render () {
-    return <div>App12312345</div>;
+    return (
+      <Router>
+        <div>
+          <Header />
+          <Route exact path='/' component={Home} />
+          <Route path='/about' component={About} />
+        </div>
+      </Router>
+    );
   }
 }
 
