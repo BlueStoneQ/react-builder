f9a7140accd2514762ed5959fdf643707845ee6c
qiaoyang <qiaoyang@bonc.com.cn>
2019-05-05 15:06:37 +0800

    HMR - React-hot-loader的配置和使用

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import { hot } from 'react-hot-loader/root';
 
 class App extends Component {
   render () {
-    return <div>App</div>;
+    return <div>App12312345</div>;
   }
 }
 
-export default App;
+export default hot(App);
