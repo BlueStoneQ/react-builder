d34b3ff0833653f14d2af04689ccc7c1db4132bf
qiaoyang <qiaoyang@bonc.com.cn>
2019-05-07 17:44:02 +0800

    publicPath的分环境设置 + 使用node启动dev-server

--- build/config.js
+++ build/config.js
@@ -3,11 +3,15 @@
  * 1- 公共路径配置
  */
 const path = require('path');
+const IS_DEV = process.env.NODE_ENV === 'development';
+
+const ROOT_PATH = path.resolve(__dirname, '../');
 
 module.exports = {
   PORT: 3002,
-  ROOT_PATH: path.resolve(__dirname, '../'),
-  SRC_PATH: path.resolve(__dirname, '../', 'src'),
-  BUILD_PATH: path.resolve(__dirname, '../', 'dist'), // 打包后结果路径
-  PUBLIC_DIR: path.resolve(__dirname, '../', 'public') // 静态资源实际路径
+  ROOT_PATH: ROOT_PATH,
+  SRC_PATH: path.resolve(ROOT_PATH, 'src'),
+  BUILD_PATH: path.resolve(ROOT_PATH, 'dist'), // 打包后结果路径
+  PUBLIC_DIR: path.resolve(ROOT_PATH, 'public'), // 静态资源实际路径
+  // PUBLIC_PATH: IS_DEV ? ROOT_PATH : ROOT_PATH // 目前dev和prod环境下。publlicpath都设置为根目录 
 };
