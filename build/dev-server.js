44844be249b26947bcf4d62cd82a3527648a54e1
qiaoyang <qiaoyang@bonc.com.cn>
2019-05-07 16:37:32 +0800

    新功能-增加对打包后提示信息-webpack-dev-server中的waitUntilValid

--- build/dev-server.js
+++ build/dev-server.js
@@ -5,9 +5,10 @@ const path = require("path");
 const express = require("express");
 const webpack = require("webpack");
 const config = require('./config');
-const webpackDevMiddleware = require("webpack-dev-middleware");
-const webpackHotMiddleware = require("webpack-hot-middleware");
+const WebpackDevMiddleware = require("webpack-dev-middleware");
+const WebpackHotMiddleware = require("webpack-hot-middleware");
 const open = require('open');
+const chalk = require('chalk');
 
 let webpackConfig = require('./webpack.dev.config');
 
@@ -16,29 +17,37 @@ webpackConfig.entry.push('webpack-hot-middleware/client');
 
 const compiler = webpack(webpackConfig);
 
+// webpack-dev-middleware instance
+const devMiddleware = WebpackDevMiddleware(compiler, {
+  noInfo: true,
+});
+
+// webpack-hot-middleware instance
+const hotMiddleware = WebpackHotMiddleware(compiler, {
+  noInfo: true,
+});
+
 const PORT = config.PORT;
 const URL = `http://localhost:${PORT}`;
 
 const app = express();
  
-app.use(
-  webpackDevMiddleware(compiler, {
-    noInfo: true,
-  })
-);
+app.use(devMiddleware);
 
-app.use(webpackHotMiddleware(compiler, {
-  noInfo: true,
-}));
+app.use(hotMiddleware);
 
 // 设置访问静态文件的路径
 // app.use(express.static(DIST_DIR));
  
-module.exports = app.listen(PORT, (err) => {
+module.exports = app.listen(PORT);
+
+// 编译完成后回调 https://github.com/webpack/webpack-dev-middleware#waituntilvalidcallback
+devMiddleware.waitUntilValid(function() {
+  // 打包成功后作出提示
+  console.log(chalk.cyan(`- Local: ${URL}`))
   // 打开默认浏览器
   open(URL);
-  console.log(`- Local: ${URL}`)
-});
+})
 
 
 
