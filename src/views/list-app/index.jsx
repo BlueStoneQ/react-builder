cdaf569d4d361fc9db316d945cd332c5ac20f4eb
qiaoyang <qiaoyang@bonc.com.cn>
2019-06-28 15:56:47 +0800

    引入styled-component - 初阶使用

--- src/views/list-app/index.jsx
+++ src/views/list-app/index.jsx
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
 import style from './ListApp.less';
+import styled from 'styled-components';
+
+const Item =  styled.li`
+  width: 200px;
+  height: 30px;
+  &:nth-child(odd) {
+    background: rgba(250, 250, 0, 0.4)
+  }
+  &:nth-child(even) {
+    background: rgba(0, 250, 250, 0.4);
+  }
+  &:hover {
+    background: rgba(250, 0, 0, 0.4);
+  }
+`;
 
 class ListApp extends Component {
   render () {
     return (
-      <span className={style.listWrap}>
-        List123
-      </span>
+      <ul className={style.listWrap}>
+        {[1, 2, 3, 4, 5].map((v, i) => (
+          <Item key={v+i}>{v}</Item>
+        ))}
+      </ul>
     );
   }
 }
