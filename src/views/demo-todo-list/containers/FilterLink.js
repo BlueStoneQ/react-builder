c95b1e79aebebf5d372cc9ba35f66adb49a04d16
乔阳 <sTone@jinxianshengdeMacBook-Pro.local>
2019-09-16 13:13:56 +0800

    demo-增加部分函数注释

--- src/views/demo-todo-list/containers/FilterLink.js
+++ src/views/demo-todo-list/containers/FilterLink.js
@@ -5,12 +5,22 @@ import { connect } from 'react-redux';
 import { setVisibilityFilter } from '$redux/actions/demo-todo-list';
 import Link from '../components/Link';
 
+/**
+ * 从redux store中读取state
+ * @param {*} state 
+ * @param {*} ownProps 
+ */
 const mapStateToProps = (state, ownProps) => {
   return {
     active: ownProps.filter === state.visibilityFilter
   };
 };
 
+/**
+ * 写入redux store中
+ * @param {*} dispatch 
+ * @param {*} ownProps 
+ */
 const mapDispatchToProps = (dispatch, ownProps) => {
   return {
     onClick: () => {
